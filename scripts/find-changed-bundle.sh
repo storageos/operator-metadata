9da65015075d7ec1b5ed2bff5ad3e2c3c75bc429
Sunny <darkowlzz@protonmail.com>
2020-10-20 22:02:13 +0530

    Find changed bundle and test that bundle only
    Adds find-changed-bundle.sh script to identify the changed versioned
    bundle. Run e2e test against the changed bundle only.
    
    Disable the e2e on master branch. Run it for PRs against master branch
    only, where only one versioned bundle changes.

---
+++ scripts/find-changed-bundle.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This script tries to identified the versioned bundle changed in a pull
+# request. This can be used to run tests against a specific versioned bundle.
+# Based on operator-framework community-operator CI setup:
+#   https://github.com/operator-framework/community-operators/blob/d1db9ac3bb8b8e241baf66fcab108d37c9caf407/scripts/ci/ansible-env
+
+unset BUNDLE_PATH
+unset BUNDLE_VERSION
+
+PR_COMMIT=$(git log -n1 --format=format:"%H" | tail -n 1)
+
+for file in $(git log -m -1 --name-only --first-parent $PR_COMMIT --name-only 2>&1); do
+	if echo "$file" | grep -q "^storageos2"; then
+		# Bundle paths are three level deep: storageos2/<version>/metadata/...
+		if [ $(echo $file| awk -F'/' '{print NF}') -ge 3 ]; then
+			BUNDLE_VERSION="$(echo "$file" | awk -F'/' '{ print $2 }')"
+			BUNDLE_PATH="$(echo "$file" | awk -F'/' '{ print $1"/"$2 }')"
+		fi
+	fi
+done
